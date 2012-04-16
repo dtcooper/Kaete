bff01b238924ae8060e205ef4065e7f39b39c222
David Cooper <dcooper@trapeze.com>
2012-04-16 11:34:44 -0400

    Linted kaete.js

--- kaete/kaete.js
+++ kaete/kaete.js
@@ -13,7 +13,7 @@
             this.func = cache[template].func;
             this.func_body = cache[template].func_body;
         }
-    }
+    };
 
     // The beginning special char, eg the "[" in "[% ... %]"
     Kaete.TAG_START = '[';
@@ -47,39 +47,40 @@
 
     var generate_parser = function() {
         var escape_regexp = function(s) {
-            return s.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-        }
+            return s.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
+        };
 
         parser = new RegExp(
             // Start of tag, start of non-capturing group for tag types.
-            escape_regexp(Kaete.TAG_START) + '(?:'
+            escape_regexp(Kaete.TAG_START) + '(?:' +
 
-            // Match a code tag, group 1 as matched code, end of code tag;
-            + escape_regexp(Kaete.TAG_CODE_START) + '([\\s\\S]+?)' + escape_regexp(Kaete.TAG_CODE_END)
+                // Match a code tag, group 1 as matched code, end of code tag;
+                escape_regexp(Kaete.TAG_CODE_START) + '([\\s\\S]+?)' + escape_regexp(Kaete.TAG_CODE_END) +
 
-            // or match a variable tag, group 2 as the optional unescape char,
-            + '|' + escape_regexp(Kaete.TAG_VARIABLE_START) + '(' + escape_regexp(Kaete.TAG_VARIABLE_UNESCAPED) + '?)'
+                // or match a variable tag, group 2 as the optional unescape char,
+                '|' + escape_regexp(Kaete.TAG_VARIABLE_START) + '(' + escape_regexp(Kaete.TAG_VARIABLE_UNESCAPED) + '?)' +
 
-            // group 3 as matched expression, end of variable tag;
-            + '([\\s\\S]+?)' + escape_regexp(Kaete.TAG_VARIABLE_END)
+                // group 3 as matched expression, end of variable tag;
+                '([\\s\\S]+?)' + escape_regexp(Kaete.TAG_VARIABLE_END) +
 
-            // or match a complete comment tag, no need to group
-            + '|' + escape_regexp(Kaete.TAG_COMMENT_START) + '[\\s\\S]+?' + escape_regexp(Kaete.TAG_COMMENT_END)
+                // or match a complete comment tag, no need to group
+                '|' + escape_regexp(Kaete.TAG_COMMENT_START) + '[\\s\\S]+?' + escape_regexp(Kaete.TAG_COMMENT_END) +
 
-            // end of non-capturing group, end of tag, end of rexexp (with global flag)
-            + ')' + escape_regexp(Kaete.TAG_END), 'g');
-
-    }
+                // end of non-capturing group, end of tag, end of rexexp (with global flag)
+                ')' + escape_regexp(Kaete.TAG_END),
+            'g'
+        );
+    };
 
     var print_array_name = '__Kaete_print';
     var context_name = '__Kaete_context';
 
     var escape_html_regexp = /[&<>"'\/]/g;
     Kaete.escape_html = function(s) {
-        return ("" + s).replace(escape_html_regexp, function(match) {
+        return (String(s)).replace(escape_html_regexp, function(match) {
             return '&#' + match.charCodeAt(0) + ';';
         });
-    }
+    };
 
 
     Kaete.prototype.compile = function() {
@@ -101,7 +102,7 @@
                     statements.push(",\n");
                 } else {
                     print_mode = true;
-                    statements.push(print_array_name, ".push(")
+                    statements.push(print_array_name, ".push(");
                 }
 
                 if (!unescaped) {
@@ -114,13 +115,13 @@
                     statements.push(')');
                 }
             }
-        }
+        };
 
         var compile_string = function(string) {
             if (string) {
                 compile_variable(JSON.stringify(string), true);
             }
-        }
+        };
 
         var compile_code = function(code) {
             if (code) {
@@ -131,14 +132,15 @@
 
                 statements.push(code, "\n");
             }
-        }
+        };
 
         // Build function beginning
         statements.push(
             "var ", print_array_name, " = [];\n",
             "var print_unescaped = function() { ", print_array_name, ".push.apply(", print_array_name, ", arguments); };\n",
             "var print = function() { for (var i = 0; i < arguments.length; i++) { ", print_array_name, ".push(Kaete.escape_html(arguments[i])); }; };\n",
-            "with (", context_name, ") {\n");
+            "with (", context_name, ") {\n"
+        );
 
         // Walk through template looking for template tags
         var match;
@@ -168,7 +170,7 @@
         } catch (exception) {
             this.func = function() { return "<p>Error compiling template: " + Kaete.escape_html(exception.message) + "</p>"; };
         }
-    }
+    };
 
     Kaete.prototype.render = function(context) {
         // Call with global scope
@@ -177,27 +179,30 @@
         } catch (exception) {
             return "<p>Error rendering template: " + Kaete.escape_html(exception.message) + "</p>";
         }
-    }
+    };
 
     Kaete.render = function(template, context) {
         var t = new Kaete(template);
         return t.render(context);
-    }
+    };
 
     Kaete.configure = function(options) {
         if (options) {
-            for (var option in options) {
-                // make sure it's uppercase in a naive attempt to safeguard the namespace
-                if (/^[A-Z_0-9]+$/.test(option) && (typeof Kaete[option] !== "undefined")) {
-                    Kaete[option] = options[option];
-                } else {
-                    alert("Kaete.configure(): Invalid option. [" + option + "="
-                        + JSON.stringify(options[option]) + "]");
+            var option;
+            for (option in options) {
+                    if (options.hasOwnProperty(option)) {
+                    // make sure it's uppercase in a naive attempt to safeguard the namespace
+                    if (/^[A-Z_0-9]+$/.test(option) && (typeof Kaete[option] !== "undefined")) {
+                        Kaete[option] = options[option];
+                    } else {
+                        alert("Kaete.configure(): Invalid option. [" + option +
+                            "=" + JSON.stringify(options[option]) + "]");
+                    }
                 }
             }
         }
 
         generate_parser();
-    }
+    };
 
 }).call(this);
